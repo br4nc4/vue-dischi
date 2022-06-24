a85cde5678322ee5ae48f9761a517f7def112dc5
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-24 18:32:59 +0200

    cards styling working pt.1

--- src/components/SpotifyCard.vue
+++ src/components/SpotifyCard.vue
@@ -1,9 +1,14 @@
 <template>
-    <div class="cardBg text-center">
-        <img class="myImg" :src="info.poster" :alt="info.title">
-        <h5 class="text-white text-uppercase">{{info.title}}</h5>
-        <div class="text-secondary">{{info.author}}</div>
-        <div class="text-secondary">{{info.year}}</div>
+    <div class="myCard h-100">
+        <div>
+            <img class="myImg" :src="info.poster" :alt="info.title">
+        </div>
+
+        <div class="text-center">
+            <h5 class="text-white text-uppercase">{{info.title}}</h5>
+            <p class="text-secondary">{{info.author}}</p>
+            <p class="text-secondary">{{info.year}}</p>
+        </div>
     </div>
 </template>
 
@@ -17,16 +22,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .cardBg{
+    .myCard{
         background-color: #2e3a46;
         font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-        height: 100%;
-        width: 200px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 180px;
         padding: 1rem 1rem;
+        cursor: pointer;
     }
 
     .myImg{
         aspect-ratio: 1/1;
-        width: 140px;
+        width: 100%;
     }
 </style>
